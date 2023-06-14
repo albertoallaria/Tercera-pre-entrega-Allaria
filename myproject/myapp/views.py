ed1e977353a073f66f73c4d8e184bcf6a33fdcf4
Alberto Allaria <albertoallaria@gmail.com>
2023-06-13 21:41:38 -0300

    algo

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,33 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import Usuario
 
+def usuarios(request):
+    # Aquí va la lógica para manejar la solicitud de la ruta /usuarios/
+    # Puedes agregar el código necesario para mostrar o procesar los datos de los usuarios.
+    # Por ejemplo:
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios.html', {'usuarios': usuarios})
+
 def registrar_usuario(request):
     if request.method == 'POST':
-        nombre = request.POST['nombre']
-        email = request.POST['email']
-        password = request.POST['password']
-        telefono = request.POST['telefono']
-        direccion_facturacion = request.POST['direccion_facturacion']
-        usuario = Usuario(nombre=nombre, email=email, password=password, telefono=telefono, direccion_facturacion=direccion_facturacion)
-        usuario.save()
+        # Código para registrar un nuevo usuario
         return redirect('inicio')
     return render(request, 'registrar_usuario.html')
 
 def iniciar_sesion(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        try:
-            usuario = Usuario.objects.get(email=email, password=password)
-            return redirect('inicio')
-        except Usuario.DoesNotExist:
-            mensaje = 'El correo electrónico o la contraseña son incorrectos'
-            return render(request, 'iniciar_sesion.html', {'mensaje': mensaje})
+        # Código para iniciar sesión
+        return redirect('inicio')
     return render(request, 'iniciar_sesion.html')
-
-
-from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,16 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from myapp.views import usuarios, index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.urls import path
-from myapp.views import usuarios
-
-urlpatterns = [
+    path('', index, name='inicio'),  # Ruta para la URL raíz
     path('usuarios/', usuarios, name='usuarios'),
+    # Otras rutas de tu proyecto...
 ]
